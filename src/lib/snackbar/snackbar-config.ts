6f16fe24a7606f176bfc33e465fac536a6ad878d
Dominic Carretto <trimoxal@gmail.com>
2017-09-29 21:01:28 -0400

    feat: Snackbar config

--- src/lib/snackbar/snackbar-config.ts
+++ src/lib/snackbar/snackbar-config.ts
@@ -3,13 +3,16 @@ import { ViewContainerRef, InjectionToken } from '@angular/core';
 export const MDC_SNACK_BAR_DATA = new InjectionToken<any>('MdcSnackbarData');
 
 export class MdcSnackbarConfig {
-  message: string = null;
   timeout?: number = 2750;
   actionHandler?: Function;
-  actionText?: string = null;
   multiline?: boolean = false;
   actionOnBottom?: boolean = false;
-  alignStart: boolean = false;
+  align?: string = 'center';
+  dismissOnAction?: boolean = true;
+  focusAction?: boolean = false;
+
+  /** Data being injected into the child component. */
+  data?: any = null;
 
   /** The view container to place snack bar into. */
   viewContainerRef?: ViewContainerRef;
